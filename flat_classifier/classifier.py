514eb05cc8e149b8c21a4f31e5a91d16399f91ad
ismylizin <60669633+ismylizin@users.noreply.github.com>
2020-10-04 11:32:18 -0300

    Resultados SVM

--- flat_classifier/classifier.py
+++ flat_classifier/classifier.py
@@ -31,12 +31,12 @@ from hierarchical_classifier.results.global_results_framework import GlobalResul
 
 
 def main():
-    data = './feature_extraction/result/filtered_covid_canada_rydles_plus7.csv'
-    classifier_name = 'rf'
+    data = '../feature_extraction/result/filtered_covid_canada_rydles_plus7.csv'
+    classifier_name = 'svm'
     n_experiment_runs = 20
     n_folds = 5
     k_neighbors = 5
-    result_path = './flat_classifier/results/detailed'
+    result_path = './results/detailed'
 
     global_config = GlobalConfig.instance()
     global_config.set_kneighbors(k_neighbors)
@@ -51,8 +51,8 @@ def main():
 
     run_all_experiments(n_experiment_runs, result_path, output_data, input_data, kfold, classes, classifier_name)
 
-def train_and_predict(train_index, test_index, input_data, output_data, resampling_algorithm_name, classifier_name):
 
+def train_and_predict(train_index, test_index, input_data, output_data, resampling_algorithm_name, classifier_name):
     inputs_train, outputs_train = input_data[train_index], output_data[train_index]
     inputs_test, outputs_test = input_data[test_index], output_data[test_index]
 
@@ -63,14 +63,14 @@ def train_and_predict(train_index, test_index, input_data, output_data, resampli
         resampler = resampling_algorithm.instantiate_resampler('flat', resampling_algorithm_name, 5)
 
         input_data_train, output_data_train = resampler.fit_resample(inputs_train,
-                                                                    outputs_train)
+                                                                     outputs_train)
         print("Done resampling")
     else:
         input_data_train = inputs_train
         output_data_train = outputs_train
 
     print("Started training ", end='')
-    
+
     clf = some_functions.get_classifier(classifier_name)
     clf = clf.fit(input_data_train, output_data_train)
     print("-> Finished")
@@ -81,15 +81,17 @@ def train_and_predict(train_index, test_index, input_data, output_data, resampli
 
     return outputs_test, predicted
 
-def run_cross_validation(output_data, input_data, kfold, classes, resampling_strategy, resampling_algorithm_name, classifier_name):
 
+def run_cross_validation(output_data, input_data, kfold, classes, resampling_strategy, resampling_algorithm_name,
+                         classifier_name):
     all_folds_result_list = []
     all_folds_outputs_test = []
     all_folds_predicted = []
 
     kfold_count = 1
     for train_index, test_index in kfold.split(input_data, output_data):
-        outputs_test, predicted = train_and_predict(train_index, test_index, input_data, output_data, resampling_algorithm_name, classifier_name)
+        outputs_test, predicted = train_and_predict(train_index, test_index, input_data, output_data,
+                                                    resampling_algorithm_name, classifier_name)
 
         pipeline_results = PipeleineResultsFramework(resampling_strategy, resampling_algorithm_name)
         per_class_metrics = pipeline_results.calculate_perclass_metrics(outputs_test, predicted)
@@ -97,7 +99,8 @@ def run_cross_validation(output_data, input_data, kfold, classes, resampling_str
 
         [hp, hr, hf] = calculate_flat_metrics(predicted, outputs_test, avg_type='macro')
 
-        result = ExperimentResultDTO(ResultDTO(hp, hr, hf), per_class_metrics, conf_matrix, resampling_strategy, resampling_algorithm_name, kfold_count)
+        result = ExperimentResultDTO(ResultDTO(hp, hr, hf), per_class_metrics, conf_matrix, resampling_strategy,
+                                     resampling_algorithm_name, kfold_count)
 
         print('=' * 50)
         print('')
@@ -105,29 +108,36 @@ def run_cross_validation(output_data, input_data, kfold, classes, resampling_str
         kfold_count += 1
         all_folds_result_list.append(result)
 
-    [average_experiment_results, per_class_results_data_frame] = calculate_average_fold_result(all_folds_result_list, classes)
+    [average_experiment_results, per_class_results_data_frame] = calculate_average_fold_result(all_folds_result_list,
+                                                                                               classes)
 
-    average_result = AverageExperimentResultDTO(average_experiment_results, resampling_strategy, resampling_algorithm_name)
-    average_result_per_class = AverageExperimentResultDTO(per_class_results_data_frame, resampling_strategy, resampling_algorithm_name)
+    average_result = AverageExperimentResultDTO(average_experiment_results, resampling_strategy,
+                                                resampling_algorithm_name)
+    average_result_per_class = AverageExperimentResultDTO(per_class_results_data_frame, resampling_strategy,
+                                                          resampling_algorithm_name)
 
     return [average_result, average_result_per_class]
 
 
-
 def run_all_experiments(n_experiment_runs, result_path, output_data, input_data, kfold, classes, classifier_name):
-    for experiment_index in range(1, n_experiment_runs+1):
-        
+    for experiment_index in range(1, n_experiment_runs + 1):
+
         results_list = []
         results_per_class_list = []
-        create_result_directories(result_path + "_exp" + str(experiment_index), resampling_algorithm.resampling_strategies, resampling_algorithm.resampling_algorithms)
+        create_result_directories(result_path + "_exp" + str(experiment_index),
+                                  resampling_algorithm.resampling_strategies,
+                                  resampling_algorithm.resampling_algorithms)
 
         for res, resampling_algorithm_name in enumerate(resampling_algorithm.resampling_algorithms):
             if resampling_algorithm_name == resampling_algorithm.NONE:
                 resampling_strategy = resampling_algorithm.NONE
             else:
                 resampling_strategy = resampling_algorithm.FLAT_RESAMPLING
-            [average_result, average_result_per_class] = run_cross_validation(output_data, input_data, kfold, classes, resampling_strategy, resampling_algorithm_name, classifier_name)
-            
+            [average_result, average_result_per_class] = run_cross_validation(output_data, input_data, kfold, classes,
+                                                                              resampling_strategy,
+                                                                              resampling_algorithm_name,
+                                                                              classifier_name)
+
             results_list.append(average_result)
             results_per_class_list.append(average_result_per_class)
 
